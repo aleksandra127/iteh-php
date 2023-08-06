8d8c01354fd95cb2e4e3e46ec46458ef5068b14c
aleksandra127 <akojadinovic04@gmail.com>
2023-08-06 14:19:14 +0200

    kreiran dbbroker

---
+++ databasebroker.php
@@ -0,0 +1,12 @@
+<?php
+ 
+     
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "skola_stranih_jezika";
+        
+        $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
+     
+
+?>
